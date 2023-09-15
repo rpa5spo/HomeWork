e9b4649ccc590557c9c17380626801926089745c
rpa5spo <124982019+rpa5spo@users.noreply.github.com>
2023-09-15 16:55:59 +0300

    Add files via upload

--- Design-Agency/js/main.js
+++ Design-Agency/js/main.js
@@ -1,12 +1,22 @@
 $(function () {
-  
+
   $('.testimonials__slider').slick({
-    arrows: true,
+    arrows: false,
     slidesToShow: 3,
     infinite: true,
     draggable: false,
+    appendArrows: $('.testimonials__slider-arrows'),
     variableWidth: true,
-  });
-
+    waitForAnimate:false,
   })
+  $('.testimonials__slider-prev').on('click', function(e) {
+    e.preventDefault()
+    $('.testimonials__slider').slick('slickPrev')
+  })
+  $('.testimonials__slider-next').on('click', function(e) {
+    e.preventDefault()
+  $('.testimonials__slider').slick('slickNext')
+  })
+
+})
 
